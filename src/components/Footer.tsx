7cea047d9c0b0d55049e0067f68aa4de021adf25
Samri Mucheye <samrimucheye@gmail.com>
2025-05-05 16:44:17 +0000

    made changed

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ const Footer = () => {
   return (
     <footer className="bg-background border-t shadow-sm mt-8">
       <div className="container mx-auto p-4 text-center">
-        <p>&copy; {new Date().getFullYear()} AffiliateLink Hub. All rights reserved.</p>
+        <p>&copy; {new Date().getFullYear()} AffiliateLink Hub. All rights reserved always.</p>
       </div>
     </footer>
   );
